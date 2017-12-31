51af5cff612c8eb1d7ed18ae3ea80be5e5f492e7
Matti Hänninen <matti@mjhanninen.com>
2017-12-31 05:32:29 +0200

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject codebrutale/lein-template "0.1.0"
+(defproject codebrutale/lein-template "0.1.1-SNAPSHOT"
   :description "A Leiningen template customized for Code Brutale"
   :url "https://github.com/codebrutale/lein-template"
   :license {:name "Eclipse Public License"
