d36bbf747a05ed42e0e971934f02d6ccf66613bf
Matti Hänninen <matti@mjhanninen.com>
2017-12-31 06:57:51 +0200

    Deploy to Clojars by default

--- resources/leiningen/new/codebrutale/project.clj
+++ resources/leiningen/new/codebrutale/project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/codebrutale/{{name}}"
   :license {:name "Eclipse Public License"
             :url "https://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :deploy-repositories [["snapshots" :clojars]
+                        ["releases" :clojars]])
