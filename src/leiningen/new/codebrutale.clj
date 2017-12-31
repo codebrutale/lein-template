88bc233ff8a1afae8ece6c8b793f4d25a59ac7e2
Matti Hänninen <matti@mjhanninen.com>
2017-12-31 05:45:36 +0200

    Fix ns

--- src/leiningen/new/codebrutale.clj
+++ src/leiningen/new/codebrutale.clj
@@ -1,16 +1,15 @@
-(ns leiningen.new.lein-template
+(ns leiningen.new.codebrutale
   (:require
    [leiningen.new.templates :refer [renderer name-to-path ->files]]
    [leiningen.core.main :as main]))
 
-(def render (renderer "lein-template"))
+(def render (renderer "codebrutale"))
 
-(defn lein-template
+(defn codebrutale
   "Generate a simple Clojure project"
   [name]
   (let [data {:name name
               :sanitized (name-to-path name)}]
-    (main/info "Generating fresh 'lein new' codebrutale/lein-template project.")
     (->files data
              ["src/codebrutale/{{sanitized}}/core.clj" (render "core.clj" data)]
              ["project.clj" (render "project.clj" data)])))
