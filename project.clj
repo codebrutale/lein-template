e0aa313546bb669865f09b87bc4dbd059428dc59
Matti Hänninen <matti@mjhanninen.com>
2017-12-31 06:17:31 +0200

    Set Clojars as the default repository for deployments

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/codebrutale/lein-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :deploy-repositories [["snapshots" :clojars]
+                        ["releases" :clojars]])
